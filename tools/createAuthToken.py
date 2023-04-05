13a1a2eabb29881269655edf924b097f0a6a1605
Robin Schäfer <r-schaefer@outlook.com>
2023-04-06 01:01:17 +0200

    Added createAuthToken() tool

---
+++ tools/createAuthToken.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+
+def createAuthToken():
+    ip = input('Enter the IP: ')
+    input('The power button on the device should be held for 5-7 seconds, then you need to press enter within 30 seconds.')
+    response = requests.post(f'http://{ip}:16021/api/v1/new')
+    data = json.loads(response.text)
+    print('auth_token created:', data['auth_token'])
+
+
+createAuthToken()

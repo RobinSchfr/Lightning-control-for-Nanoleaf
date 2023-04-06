0ef979afe8e65c7d1754e6c6c45961ac42aec38a
Robin Schäfer <r-schaefer@outlook.com>
2023-04-06 14:21:38 +0200

    Removed comments with links

--- src/colors_1000.py
+++ src/colors_1000.py
@@ -1,4 +1,3 @@
-# https://github.com/Jam3/nice-color-palettes/blob/master/1000.json
 palettes = [
     ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"],
     ["#fe4365", "#fc9d9a", "#f9cdad", "#c8c8a9", "#83af9b"],

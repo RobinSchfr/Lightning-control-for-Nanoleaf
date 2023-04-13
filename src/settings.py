41149d38a0f419aed53b37ef046b673fcac60835
Robin Schäfer <r-schaefer@outlook.com>
2023-04-13 18:50:11 +0200

    Moved settings.json to src/

--- src/settings.py
+++ src/settings.py
@@ -4,15 +4,15 @@ import json
 class Settings:
     @staticmethod
     def setValue(key, value):
-        with open('../settings.json', 'r') as settingsFile:
+        with open('settings.json', 'r') as settingsFile:
             settings = json.load(settingsFile)
         settings[key] = value
-        with open('../settings.json', 'w') as settingsFile:
+        with open('settings.json', 'w') as settingsFile:
             json.dump(settings, settingsFile, indent=4)
 
     @staticmethod
     def getValue(key):
-        with open('../settings.json', 'r') as settingsFile:
+        with open('settings.json', 'r') as settingsFile:
             settings = json.load(settingsFile)
         return settings[key]
 
@@ -22,5 +22,5 @@ class Settings:
             Settings.getValue("ip")
         except FileNotFoundError:
             settings = {"ip": None, "auth_token": None, "dark_mode": True, "accent_color": "#4500db", "language": "english"}
-            with open('../settings.json', 'w') as settingsFile:
+            with open('settings.json', 'w') as settingsFile:
                 json.dump(settings, settingsFile, indent=4)

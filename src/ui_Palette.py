cd982d744b9deabe5104dd74dffa95b3b310ab62
Robin Schäfer <r-schaefer@outlook.com>
2023-04-14 18:45:02 +0200

    Color palette color field aspect ratio 1/1

--- src/ui_Palette.py
+++ src/ui_Palette.py
@@ -7,7 +7,7 @@ class Ui_Palette:
         self.id = id
         self.editPalette = editPalette
         self.ui_Structure = ui_Structure
-        uiPalette = ui.html(f'<div class="palette" style="display: flex; flex-direction: row;margin-top: 10px;filter: none;"><div style="width: 20%; height: 100px; background-color: {palette[0]};"></div><div style="width: 20%; height: 100px; background-color: {palette[1]};"></div><div style="width: 20%; height: 100px; background-color: {palette[2]};"></div><div style="width: 20%; height: 100px; background-color: {palette[3]};"></div><div style="width: 20%; height: 100px; background-color: {palette[4]};"></div></div>')
+        uiPalette = ui.html(f'<div class="palette" style="display: flex; flex-direction: row;margin-top: 10px;"><div style="width: 20%; aspect-ratio : 1 / 1; background-color: {palette[0]};"></div><div style="width: 20%; aspect-ratio : 1 / 1; background-color: {palette[1]};"></div><div style="width: 20%; aspect-ratio : 1 / 1; background-color: {palette[2]};"></div><div style="width: 20%; aspect-ratio : 1 / 1; background-color: {palette[3]};"></div><div style="width: 20%; aspect-ratio : 1 / 1; background-color: {palette[4]};"></div></div>')
         uiPalette.on('click', lambda: self.clickEvent())
 
     def clickEvent(self):
